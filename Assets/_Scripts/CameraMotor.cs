7443f7b039bbb9ed0c59e5d9ab50074e3e39d2a8
lkallab <lkallab@uwo.ca>
2022-03-22 17:11:17 -0400

    Adding Player/Camera

--- Assets/_Scripts/CameraMotor.cs
+++ Assets/_Scripts/CameraMotor.cs
@@ -5,40 +5,42 @@ using UnityEngine;
 public class CameraMotor : MonoBehaviour
 {
     public Transform lookAt;
-    public float boundX = 10f;
-    public float boundY = 5f;
+    //boundary of x and y for camera to track player 
+    public float boundX = 0.15f;
+    public float boundY  = 0.05f;
 
-    // late update to make sure everything else happens before we move the cam
     private void LateUpdate()
     {
-        Vector3 delta = new Vector3(0, 0, 0);
-
+        Vector3 delta = Vector3.zero;
+        //check if within x boundary
         float deltaX = lookAt.position.x - transform.position.x;
         if(deltaX > boundX || deltaX < -boundX)
         {
             if(transform.position.x < lookAt.position.x)
             {
-                delta.x = deltaX - boundX;
+                delta.x  = deltaX - boundX;
+                
             }
             else
             {
-                delta.x = deltaX + boundX;
+                delta.x  = deltaX + boundX;
             }
         }
 
+        //check if within y boundary
         float deltaY = lookAt.position.y - transform.position.y;
-        if (deltaY > boundY || deltaY < -boundY)
+        if(deltaY > boundY || deltaY < -boundY)
         {
-            if (transform.position.y < lookAt.position.y)
+            if(transform.position.y < lookAt.position.y)
             {
-                delta.y = deltaY - boundY;
+                delta.y  = deltaY - boundY;
+                
             }
             else
             {
-                delta.y = deltaY + boundY;
+                delta.y  = deltaY + boundY;
             }
         }
-
         transform.position += new Vector3(delta.x, delta.y, 0);
     }
 }
--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -4,30 +4,94 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private BoxCollider2D boxCollider;
-    private Vector3 moveDelta;
-    private RaycastHit2D hit;
+   float x;
+  float y;
+  private BoxCollider2D boxCollider;
+  private Vector3 moveDelta;
+  private RaycastHit2D hit;
+  
+  private float chargeCoolDown = 2f;
+   private float lastDash;
+  private float lastDashTimer = 0.5f ;
+  private bool isCharging = false;
+  private  Vector3 lastDirection; 
+   private Vector3 moveDir ;
 
-    private int coins = 0;
+   private void Start()
+   {
+       boxCollider = GetComponent<BoxCollider2D>();
+   }
+   private void FixedUpdate()
+   {
+     
+     
+     if(!isCharging) {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+      }
+       //Reset MoveDelta
+       moveDelta = new Vector3(x,y,0);
+
+       //Swap Sprite direction, when moving
+       if(moveDelta.x > 0)
+       {
+           transform.localScale = Vector3.one;
+       }
+       else if( moveDelta.x < 0)
+       {
+            transform.localScale = new Vector3(-1,1,0);
+       }
+       moveDir = new Vector3(moveDelta.x,moveDelta.y, 0);
+
+       hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Block"));
+       if(hit.collider == null)
+       {
+           //move object in y
+         transform.Translate(0,moveDelta.y * Time.deltaTime,0);
+       
+       }
+       
+       hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x,0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Block"));
+       if(hit.collider == null)
+       {
+           //move object in x
+         transform.Translate(moveDelta.x * Time.deltaTime,0,0);
+       
+       } 
+         
+       if(Input.GetKeyDown(KeyCode.Space) && Time.time - lastDash > chargeCoolDown)
+       {
+         isCharging = true; 
+         lastDash = Time.time;
+         Charge();
+       } else if(Time.time - lastDash  < lastDashTimer) {
+         Charge();
+       } else {
+         isCharging = false;
+       }
+
+       
+     
+   }
+  
+    public void Charge()
+   {
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Block"));
+       if(hit.collider == null &&  isCharging == true)
+       {
+           //move object in y
+         transform.Translate(0,moveDelta.y * Time.deltaTime,0);
+       
+       }
+       
+       hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x,0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Block"));
+       if(hit.collider == null && isCharging == true)
+       {
+           //move object in x
+         transform.Translate(moveDelta.x * Time.deltaTime,0,0);
+       
+       } 
     
-    private void Start()
-    {
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
-
-    private void FixedUpdate() {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-
-        // reset moveDelta
-        moveDelta = new Vector3(x, y, 0);
-
-        // swap sprite direction depending on left or right movement
-        // check both to ensure that we don't turn around when we stop
-        if(moveDelta.x > 0) {
-            transform.localScale = new Vector3(1, 1, 1);
-        } else if(moveDelta.x < 0) {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-    }
+      
+   }
 }
