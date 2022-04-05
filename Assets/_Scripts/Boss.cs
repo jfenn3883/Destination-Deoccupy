381a20c700a0fa86b11f8c512ea402fa6a6a149e
jfenn3883 <45019671+jfenn3883@users.noreply.github.com>
2022-04-05 16:51:20 -0400

    +boss

---
+++ Assets/_Scripts/Boss.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : Enemy
+{
+    // getDirToPlayer
+    // stay still for a sec to telegraph
+    // charge through them for x secs
+    // wait a small period of time
+    // repeat until someone dies
+}
