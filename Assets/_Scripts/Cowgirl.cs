0415d96a74bde99d14eaff28e0aaafa06a8b22c9
jfenn3883 <45019671+jfenn3883@users.noreply.github.com>
2022-04-05 05:13:03 -0400

    changing some things, going to try a raycast

--- Assets/_Scripts/Cowgirl.cs
+++ Assets/_Scripts/Cowgirl.cs
@@ -6,10 +6,14 @@ public class Cowgirl : Player
 {
     // gun variables
     public int bullets = 6;
+    public float reloadSpeed;
+    public bool isReloading = false;
     protected int loaded;
-    protected float shotCooldown = 0.5f;
+    public float shotCooldown = 0.5f;
     protected float nextShot;
-    protected float reloadCooldown = 4f;
+    public float reloadCooldown = 4f;
+    public int gunDamage = 2;
+    
 
     // private vars
     protected RaycastHit2D[] raycast = new RaycastHit2D[10];
@@ -18,13 +22,49 @@ public class Cowgirl : Player
     {
         base.Start();
         loaded = bullets;
+        reloadSpeed = speed / 2;
     }
 
-    protected void shootGun()
+    protected override void Update()
+    {
+        inputs = GetInputs();
+
+        moveDelta = new Vector2(inputs["x"], inputs["y"]);
+
+        if (inputs["right"] != 0) shootGun(0);
+        else if (inputs["up"] != 0) shootGun(1);
+        else if (inputs["left"] != 0) shootGun(2);
+        else if (inputs["down"] != 0) shootGun(3);
+    }
+
+    protected override void move()
+    {
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = Vector2.one;
+        }
+        else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);// double check
+        }
+
+        if(isReloading)
+        {
+            hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.size, 0, new Vector2(moveDelta.x, moveDelta.y), Mathf.Abs(moveDelta.magnitude * Time.deltaTime * reloadSpeed), LayerMask.GetMask("Enemy", "Blocking"));
+            if (hit.collider == null) transform.Translate(moveDelta * Time.deltaTime * reloadSpeed);
+        } else
+        {
+            hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.size, 0, new Vector2(moveDelta.x, moveDelta.y), Mathf.Abs(moveDelta.magnitude * Time.deltaTime * speed), LayerMask.GetMask("Enemy", "Blocking"));
+            if (hit.collider == null) transform.Translate(moveDelta * Time.deltaTime * speed);
+        }
+    }
+
+    protected void shootGun(int dir)
     {
         if(Time.time > nextShot && loaded > 0)
         {
-            fireGun(0); // dir math
+            isReloading = false;
+            fireGun(dir); // dir math
             nextShot = Time.time + shotCooldown;
             loaded--;
             if (loaded < 1) reloadGun();
@@ -33,6 +73,7 @@ public class Cowgirl : Player
 
     protected void reloadGun()
     {
+        isReloading = true;
         nextShot = Time.time + reloadCooldown;
         loaded = bullets;
     }
@@ -42,5 +83,13 @@ public class Cowgirl : Player
         if(dir == 0 || dir == 2) raycast = Physics2D.BoxCastAll(boxCollider.bounds.center, new Vector2(.16f, .04f), 0, new Vector2(moveDelta.x, 0), LayerMask.GetMask("Enemy"));
         else if(dir == 1) raycast = Physics2D.BoxCastAll(boxCollider.bounds.center, new Vector2(.16f, .04f), 90, new Vector2(0, 1), LayerMask.GetMask("Enemy"));
         else if (dir == 3) raycast = Physics2D.BoxCastAll(boxCollider.bounds.center, new Vector2(.16f, .04f), 90, new Vector2(0, -1), LayerMask.GetMask("Enemy"));
+
+        foreach(RaycastHit2D hit in raycast)
+        {
+            if (hit.collider == null) continue;
+            hit.collider.gameObject.GetComponent<Enemy>().damage(gunDamage);
+        }
+
+        raycast = new RaycastHit2D[10];
     }
 }
